a6daf46c544b63015b53228dd0bc9d49622b3923
Sergei Koshel <2191338+sk-pub@users.noreply.github.com>
2024-03-16 15:37:57 +0100

    Fix work with disposed cancellation token sources

--- src/TeilOne.Threading.DistributedCancellationToken.Redis/RedisDistributedCancellationTokenSourceFactory.cs
+++ src/TeilOne.Threading.DistributedCancellationToken.Redis/RedisDistributedCancellationTokenSourceFactory.cs
@@ -1,6 +1,7 @@
 ﻿using StackExchange.Redis;
 using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,38 +9,49 @@ namespace TeilOne.Threading.DistributedCancellationToken.Redis
 {
     public sealed class RedisDistributedCancellationTokenSourceFactory : IDisposable
     {
-        private readonly ConcurrentDictionary<string, CancellationTokenSource> _cancellationTokenSources = new ConcurrentDictionary<string, CancellationTokenSource>();
+        private readonly ConcurrentDictionary<string, ConcurrentDictionary<CancellationTokenSource, bool>> _cancellationTokenSources = new ConcurrentDictionary<string, ConcurrentDictionary<CancellationTokenSource, bool>>();
         private readonly SemaphoreSlim _channelSubscribtionSemaphore = new SemaphoreSlim(1, 1);
 
         private readonly RedisChannel _channel;
         private readonly ConnectionMultiplexer _connectionMultiplexer;
 
+        private readonly DateTime _lastCleanup = DateTime.Now;
+        private readonly TimeSpan _cleanupInterval;
+        private readonly SemaphoreSlim _cleanupSemaphore = new SemaphoreSlim(1, 1);
+
         private volatile ISubscriber _channelSubscriber;
 
-        public RedisDistributedCancellationTokenSourceFactory(string configuration, string channelName)
+        public ICollection<string> ActiveCancellations => _cancellationTokenSources.Keys;
+
+        public RedisDistributedCancellationTokenSourceFactory(string configuration, string channelName, TimeSpan? cleanupInterval = null)
         {
             _connectionMultiplexer = ConnectionMultiplexer.Connect(configuration);
             _channel = RedisChannel.Literal(channelName);
+
+            _cleanupInterval = cleanupInterval ?? TimeSpan.FromSeconds(60);
         }
 
         public async Task<CancellationTokenSource> Create(string cancellationName)
         {
+            await CleanupIfNeeded();
+
             await SubscribeToChannel();
 
-            return _cancellationTokenSources.GetOrAdd(cancellationName, key =>
+            var tokensForCancellation = _cancellationTokenSources.GetOrAdd(cancellationName, key => new ConcurrentDictionary<CancellationTokenSource, bool>());
+
+            var cts = new CancellationTokenSource();
+
+            cts.Token.Register(async () =>
             {
-                var cts = new CancellationTokenSource();
-
-                cts.Token.Register(async () =>
+                if (tokensForCancellation.TryRemove(cts, out _))
                 {
-                    if (_cancellationTokenSources.TryRemove(cancellationName, out _))
-                    {
-                        await _channelSubscriber.PublishAsync(_channel, cancellationName);
-                    }
-                });
-
-                return cts;
+                    await _channelSubscriber.PublishAsync(_channel, cancellationName);
+                }
             });
+
+            tokensForCancellation.TryAdd(cts, true);
+
+            return cts;
         }
 
         public void Dispose()
@@ -65,20 +77,71 @@ namespace TeilOne.Threading.DistributedCancellationToken.Redis
 
                 _channelSubscriber = _connectionMultiplexer.GetSubscriber();
 
-                await _channelSubscriber.SubscribeAsync(_channel, (_, message) =>
+                await _channelSubscriber.SubscribeAsync(_channel, async (_, message) =>
                 {
                     var cancellationName = (string)message;
 
-                    // Remove before cancelling so that the message is not published to Redis
-                    if (_cancellationTokenSources.TryRemove(cancellationName, out var cts))
+                    if (_cancellationTokenSources.TryRemove(cancellationName, out var tokensForCancellation))
                     {
-                        cts.Cancel();
+                        await _channelSubscriber.PublishAsync(_channel, cancellationName);
+
+                        foreach (var ctsItem in tokensForCancellation)
+                        {
+                            var cts = ctsItem.Key;
+
+                            // Remove before cancelling so that the message is not published to Redis
+                            if (tokensForCancellation.TryRemove(cts, out var x))
+                            {
+                                cts.Cancel();
+                            }
+                        }
                     }
                 });
             }
             finally
             {
                 _channelSubscribtionSemaphore.Release();
+            }
+        }
+
+        private void RemoveDisposedCancellationTokenSources()
+        {
+            foreach (var tokensForCancellationItem in _cancellationTokenSources)
+            {
+                var tokensForCancellation = tokensForCancellationItem.Value;
+                foreach (var ctsItem in tokensForCancellation)
+                {
+                    var cts = ctsItem.Key;
+                    try
+                    {
+                        _ = cts.Token;
+                    }
+                    catch(ObjectDisposedException)
+                    {
+                        tokensForCancellation.TryRemove(cts, out _);
+                        if (tokensForCancellation.IsEmpty)
+                        {
+                            _cancellationTokenSources.TryRemove(tokensForCancellationItem.Key, out _);
+                        }
+                    }
+                }
+            }
+        }
+
+        private async Task CleanupIfNeeded()
+        {
+            await _cleanupSemaphore.WaitAsync();
+
+            try
+            {
+                if (DateTime.Now.Subtract(_lastCleanup) >= _cleanupInterval)
+                {
+                    RemoveDisposedCancellationTokenSources();
+                }
+            }
+            finally
+            {
+                _cleanupSemaphore.Release();
             }
         }
     }
